af005cdcabf6f4f05ea13ec1952d91f310cfbfea
Robin Hohnsbeen <robin@hohnsbeen.de>
2023-07-08 18:41:33 +0200

    Added push animation and footstep sound events

---
+++ Assets/AnimatorSounds.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorSounds : MonoBehaviour
+{
+
+    public List<AudioClip> FootstepSounds = new List<AudioClip>();
+
+    public void FootStepSound()
+    {
+        
+        Debug.Log("Footstep sound played!");
+        if (FootstepSounds.Count > 0)
+        {
+            GetComponent<AudioSource>().PlayOneShot(FootstepSounds[Random.Range(0, FootstepSounds.Count)]);
+        }
+    }
+}
