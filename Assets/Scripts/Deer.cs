5073bb0679893176111ae16197255c2f3f37b7ea
Robin Hohnsbeen <robin@hohnsbeen.de>
2023-07-09 17:38:00 +0200

    Trigger hit and death animation

--- Assets/Scripts/Deer.cs
+++ Assets/Scripts/Deer.cs
@@ -8,50 +8,13 @@ public class Deer : MonoBehaviour
     public static bool isDead = false;
     public float health = 100;
     private DeerMovement deerMovement;
-    private AudioSource audioSource;
 
-    // These are the sounds of the deer
-    public AudioClip walkingSound;
-    public AudioClip runningSound;
+    public Animator _animator;
 
-    // Start is called before the first frame update
     void Awake()
     {
         player = gameObject;
-        audioSource = GetComponent<AudioSource>();
         deerMovement = GetComponent<DeerMovement>();
-    }
-
-    void Update()
-    {
-        ManageSound();
-    }
-
-    private void ManageSound()
-    {
-        /*
-        if (DeerMovement.relativeSpeed == 0)
-        {
-            audioSource.Stop();
-        }
-        else
-        {
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
-            if (DeerMovement.relativeSpeed > 0.67f)
-            {
-                audioSource.clip = runningSound;
-                audioSource.volume = 0.75f;
-            }
-            else
-            {
-                audioSource.clip = walkingSound;
-                audioSource.volume = DeerMovement.relativeSpeed;
-            }
-        }
-        */
     }
 
     public void GetShoot(bool critical)
@@ -60,10 +23,14 @@ public class Deer : MonoBehaviour
         if (critical)
         {
             health -= 40;
+            if (_animator)
+            {
+                _animator.SetTrigger("OnHit");
+            }
         }
         if (health <= 0)
         {
-            isDead = true;
+            Die();
         }
     }
 
@@ -73,7 +40,17 @@ public class Deer : MonoBehaviour
         deerMovement.Trapped();
         if (health <= 0)
         {
-            isDead = true;
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        isDead = true;
+        if (_animator)
+        {
+            _animator.SetTrigger("OnDeath");
+            _animator.SetBool("IsDead", true);
         }
     }
 }
